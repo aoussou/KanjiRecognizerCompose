0ff9769bb06c984efc2c8480f41cc232ff82b5c8
ceo <john@talisol.com>
2022-09-28 13:46:01 +0900

    drawing canvas working

--- app/src/main/java/com/talisol/kanjirecognizercompose/MainActivity.kt
+++ app/src/main/java/com/talisol/kanjirecognizercompose/MainActivity.kt
@@ -3,41 +3,51 @@ package com.talisol.kanjirecognizercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.talisol.kanjirecognizercompose.screens.DrawingApp
 import com.talisol.kanjirecognizercompose.ui.theme.KanjiRecognizerComposeTheme
+import com.talisol.kanjirecognizercompose.viewModels.DrawingVM
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KanjiRecognizerComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+
+                val vm = viewModel<DrawingVM>()
+                val drawingState by vm.drawingState.collectAsState()
+                val currentPath by vm.currentPath.collectAsState()
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            elevation = 2.dp,
+                            backgroundColor = MaterialTheme.colors.surface,
+                            contentColor = MaterialTheme.colors.onSurface,
+                            title = {
+                                Text("Kanji Recognizer")
+                            },
+                            actions = {}
+                        )
+                    }
                 ) {
-                    Greeting("Android")
+                    DrawingApp(
+                        currentPath,
+                        drawingState,
+                        vm::onAction
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KanjiRecognizerComposeTheme {
-        Greeting("Android")
-    }
-}
